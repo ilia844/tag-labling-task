f62a717c0261afedbbf6e8e3d70f294bbb2cdfb8
ilia844 <w5332660@mail.ru>
2021-01-28 14:19:31 +0300

    feat: create component Button

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import './Button.scss';
+
+const Button = ({
+    children, onClick, className, disabled, active
+}) => {
+
+    const classes = classNames(
+        'btn',
+        className,
+        { active },
+    )
+
+    return (
+        <button
+            className={classes}
+            disabled={disabled}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    )
+}
+
+Button.propTypes = {
+    children: PropTypes.node,
+    onClick: PropTypes.func,
+    className: PropTypes.string,
+    disabled: PropTypes.bool,
+    active: PropTypes.bool,
+}
+
+Button.defaultProps = {
+    children: 'Default button',
+    onClick: () => {},
+    className: '',
+    disabled: false,
+    active: false,
+}
+
+export default Button;
--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Portal from '../Portal/Portal';
 import './Modal.scss';
+import Button from '../Button/Button';
 
 class Modal extends React.Component {
     render() {
@@ -18,8 +19,18 @@ class Modal extends React.Component {
                         </div>
 
                         <div className="modal-footer">
-                            <button className="modal-btn">Cancel</button>
-                            <button className="modal-btn">Ok</button>
+                            <Button
+                                className="modal-btn"
+                            >
+                                Cansel
+                            </Button>
+                            <Button
+                                className="modal-btn"
+                            >
+                                Ok
+                            </Button>
+                            {/*<button className="modal-btn">Cancel</button>*/}
+                            {/*<button className="modal-btn">Ok</button>*/}
                         </div>
                     </div>
 
